c84aacd0dc4db027947e971fb1ff0f5a9305f0bc
Ekim Coşkun <108937192+ekimcoskun@users.noreply.github.com>
2022-10-07 19:17:41 +0300

    Create Template for the Webserver

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,67 @@
-const express = require('express')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const app = express()
+const app = express();
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.get('/about', (req, res) => {
-    res.send('About page')
-})
+// Setup static directory serve
+app.use(express.static(publicDirectoryPath));
 
-app.get('/weather', (req, res) => {
-    res.send('Weather page')
-})
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Ekim Coskun",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Ekim Coskun",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is a help text",
+    title: "Help",
+    name: "Ekim Coskun",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Help article not found.",
+    title: "404",
+    name: "Ekim Coskun",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Page not found.",
+    title: "404",
+    name: "Ekim Coskun",
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log("Server is up on port 3000.");
+});

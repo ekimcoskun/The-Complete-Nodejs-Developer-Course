20f2278a8a42a751417232d6bee719c5a481f0f0
Ekim Coşkun <108937192+ekimcoskun@users.noreply.github.com>
2022-10-08 16:40:16 +0300

    Connec to the ui and fetch

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -16,12 +16,12 @@ app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
-// Setup static directory serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather App",
+    title: "Weather",
     name: "Ekim Coskun",
   });
 });
@@ -35,34 +35,39 @@ app.get("/about", (req, res) => {
 
 app.get("/help", (req, res) => {
   res.render("help", {
-    helpText: "This is a help text",
+    helpText: "This is some helpful text.",
     title: "Help",
     name: "Ekim Coskun",
   });
 });
 
 app.get("/weather", (req, res) => {
-  if (!req.query.adress) {
+  if (!req.query.address) {
     return res.send({
-      error: "You must provide an adress!",
+      error: "You must provide an address!",
     });
   }
-  geocode(req.query.adress, (error, { latitude, longitude, location } = {}) => {
-    if (error) {
-      return res.send({ error });
-    }
 
-    forecast(latitude, longitude, (error, forecastData) => {
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
       if (error) {
         return res.send({ error });
       }
-      res.send({
-        forecast: forecastData,
-        location,
-        adress: req.query.adress,
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
       });
-    });
-  });
+    }
+  );
 });
 
 app.get("/products", (req, res) => {
@@ -80,17 +85,17 @@ app.get("/products", (req, res) => {
 
 app.get("/help/*", (req, res) => {
   res.render("404", {
-    errorMessage: "Help article not found.",
     title: "404",
     name: "Ekim Coskun",
+    errorMessage: "Help article not found.",
   });
 });
 
 app.get("*", (req, res) => {
   res.render("404", {
-    errorMessage: "Page not found.",
     title: "404",
     name: "Ekim Coskun",
+    errorMessage: "Page not found.",
   });
 });
 

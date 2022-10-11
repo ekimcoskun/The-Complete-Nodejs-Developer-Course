b3b1b08d843fc127dc408c8a62b17aa9d3f77943
Ekim Coşkun <108937192+ekimcoskun@users.noreply.github.com>
2022-10-11 23:44:53 +0300

    Data Validation and Sanitization

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password cant contain 'password'");
+      }
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
+/*
+const me = new User({
+  name: "Ali",
+  age: 10,
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
+*/
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+/*
+const taskOne = new Task({
+  description: "Wash the Dishes",
+  completed: false,
+});
+
+taskOne
+  .save()
+  .then(() => {
+    console.log(taskOne);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
+*/
